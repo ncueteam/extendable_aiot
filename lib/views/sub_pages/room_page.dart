6db70d475f619af1e066816a2b3f307024429ec3
YUCHI <kirklan0423.gmail.com>
2025-04-25 23:49:06 +0800

    feat: Refactor RoomPage to use StatefulWidget and improve lifecycle management

--- lib/views/sub_pages/room_page.dart
+++ lib/views/sub_pages/room_page.dart
@@ -1,32 +1,39 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:extendable_aiot/services/fetch_data.dart';
+import 'package:flutter/material.dart';
+import 'package:easy_refresh/easy_refresh.dart';
 
-class RoomPage extends StatelessWidget {
+class RoomPage extends StatefulWidget {
   final String roomId;
+
+  const RoomPage({super.key, required this.roomId});
+
+  @override
+  State<RoomPage> createState() => _RoomPageState();
+}
+
+class _RoomPageState extends State<RoomPage>
+    with AutomaticKeepAliveClientMixin {
   final FetchData _fetchData = FetchData();
 
-  RoomPage({super.key, required this.roomId});
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<DocumentSnapshot>(
-      stream: _fetchData.getRoomById(roomId),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const Center(child: Text('發生錯誤'));
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        }
-
-        final roomData = snapshot.data!.data() as Map<String, dynamic>;
-
-        return Center(
-          child: Text('房間: ${roomData['name']}'),
-        );
-      },
+    super.build(context);
+    return Container(
+      child: Text("test"),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
